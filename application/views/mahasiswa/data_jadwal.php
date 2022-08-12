74c542698fff875c17b0ca6949119b77fa59417e
donisapu <42510678+donisapu@users.noreply.github.com>
2022-08-12 21:59:56 +0700

    wasdhghjgaqk

--- application/views/mahasiswa/data_jadwal.php
+++ application/views/mahasiswa/data_jadwal.php
@@ -9,7 +9,7 @@
                         <?php echo $this->session->flashdata('suces')?>
                             <br>
 
-                    <table class="table" width="100%" cellspacing="0">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                         <thead>
                             <tr>
                             <th>No</th>
