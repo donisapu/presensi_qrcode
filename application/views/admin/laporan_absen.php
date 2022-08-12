eb587e2540fd0271f6dfb2480ffa53f41dda646c
donisapu <42510678+donisapu@users.noreply.github.com>
2022-08-12 11:52:42 +0700

    wikias

--- application/views/admin/laporan_absen.php
+++ application/views/admin/laporan_absen.php
@@ -33,7 +33,7 @@
                 </div>
             </form>
             <div class="table-responsive">
-                <table class="table table-bordered" id="DataTales" width="100%" cellspacing="0">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
                             <th>No</th>
